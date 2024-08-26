175abb8a0e0f301e02b214efb4251151ce56024e
aloago <aloago@proton.me>
2024-08-26 10:45:18 +0200

    Improve SmokeModule.py to handle conflicting calls

--- lib/SmokeModule.py
+++ lib/SmokeModule.py
@@ -1,64 +1,60 @@
+import logging
 import RPi.GPIO as GPIO
 import time
 import threading
 
+logging.basicConfig(level=logging.INFO)
+
+
 class SmokeModule:
     def __init__(self, pin):
+        """Initialize a Smoke Module on a specified pin"""
         GPIO.setmode(GPIO.BCM)
-        self.terminate = False
+        self.stop_event = threading.Event()
         self.pin = pin
         GPIO.setup(self.pin, GPIO.OUT)
         self.turn_off()
+        logging.debug(f"initialised smoke module on pin {self.pin}")
 
     def turn_on(self):
+        """Turn on the smoke module until disabled"""
+        self.stop_thread()
         GPIO.setmode(GPIO.BCM)
         GPIO.output(self.pin, True)
-        print(f"GPIO pin {self.pin} is ON")
+        logging.debug(f"turned on smoke module on pin {self.pin}")
 
     def turn_on_for(self, duration):
-        self.terminate = True
-        time.sleep(0.02)
-        self.terminate = False
+        """Turn on the smoke module for a specified number of seconds"""
+        self.stop_thread()
         thread = threading.Thread(target=self._turn_on_for_thread, args=(duration,))
         thread.start()
 
     def _turn_on_for_thread(self, duration):
+        logging.debug(f"started new thread for pin {self.pin} that will stay on for {duration} seconds")
         self.turn_on()
-        print(f"GPIO pin {self.pin} will stay ON for {duration} seconds")
         start_time = time.time()
         while time.time() - start_time < duration:
-            if self.terminate == True:
-                print(f"Stopping the thread prematurely after {time.time() - start_time} seconds.")
+            if self.stop_event.is_set():
+                logging.debug(f"stopping the thread for {self.pin} prematurely after {time.time() - start_time} seconds because a second thread was started")
                 break
-            time.sleep(0.01)  # Sleep a bit to avoid busy-waiting
+            time.sleep(0.001)
 
         self.turn_off()
 
     def turn_off(self):
+        """Turn off the module until enabled"""
+        self.stop_thread()
         GPIO.setmode(GPIO.BCM)
         GPIO.output(self.pin, False)
-        print(f"GPIO pin {self.pin} is OFF")
+        logging.debug(f"turned off smoke module on pin {self.pin}")
+    
+    def stop_thread(self):
+        """Stop thread by starting event"""
+        self.stop_event.set()
+        time.sleep(0.002)
+        self.stop_event.clear()
 
     def cleanup(self):
+        """Clean up GPIO pins used"""
         GPIO.cleanup()
-        print("GPIO cleanup done.")
-
-"""# Example usage:
-if __name__ == "__main__":
-    # Create a SmokeModule object controlling GPIO pin 17
-    smoke_module = SmokeModule(17)
-    smoke_module2 = SmokeModule(27)
-    
-    smoke_module.turn_on()
-    smoke_module2.turn_on()
-
-    time.sleep(3)
-
-    smoke_module.turn_off()
-    smoke_module2.turn_off()
-
-    time.sleep(2)
-    
-    smoke_module.turn_on_for(5)
-    time.sleep(4)
-    smoke_module.turn_on_for(5)"""
+        logging.debug(f"cleaned up GPIOs")
