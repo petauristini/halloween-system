e16f978b3c89c2243a5acc458a097e5bff5f84fb
aloago <aloago@proton.me>
2024-10-30 22:22:27 +0100

    add gpio to speaker

--- speaker.py
+++ speaker.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from audioplayer.server import AudioPlayerServer
 from audiostreaming.output import AudioStreamingOutputServer
+from raspigpio.server import GPIOHandlerServer
 
 
 app = Flask(__name__)
 audio_player = AudioPlayerServer(app)
 streaming_output = AudioStreamingOutputServer(app)
+gpio_handler_server = GPIOHandlerServer(app)
+gpio_handler_server.add(2)
+
 app.run(debug=True, host='0.0.0.0', port=5001)
