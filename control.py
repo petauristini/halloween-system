ba69a5afc1f64866b1fbd3fcf17cf4d2453896fc
--replace-all <AndrinWinzap@proton.me>
2024-10-30 10:07:07 +0100

    add new devices

--- control.py
+++ control.py
@@ -1,29 +1,56 @@
 from trigger import TriggerHandler
-from audioplayer.api import AudioServer, AudioServerGroup
+from audioplayer.api import AudioPlayer, AudioPlayerGroup
 from flask import Flask
 import logging
 from wled.api import Wled, WledGroup
+from raspigpio.api import GPIOPin, GPIOPinGroup 
 
 flask_logger = logging.getLogger('werkzeug')
 flask_logger.setLevel(logging.WARNING)
-
 root_logger = logging.getLogger('root')
 root_logger.setLevel(logging.WARNING)
 
 app = Flask(__name__)
-handler = TriggerHandler(app)
-server = AudioServer('localhost', 5001)
-server2 = AudioServer('192.168.6.240', 5000)
-all_servers = AudioServerGroup ([server, server2])
-wled = Wled('192.168.6.182')
-handler.add('trigger1')
-handler.add_callback('trigger1', 'callback1', (all_servers.play, ('joghurt', )))
-handler.add('stopall', deactivate_cooldown=True)
-handler.add_callback('stopall', 'stopall_callback', (all_servers.stop, ('joghurt', )))
-handler.add('httptest')
-handler.add_http_callback("httptest", "httptest_callback", "http://192.168.6.151/trigger/trigger1")
-handler.add('wled_on', deactivate_cooldown=True)
-handler.add_callback('wled_on', 'wled_callback', (wled.on, ()))
-handler.add('wled_off', deactivate_cooldown=True)
-handler.add_callback('wled_off', 'wled_callback', (wled.off, ()))
-app.run(debug=False, host='0.0.0.0')
+trigger_handler = TriggerHandler(app)
+
+# Audio Players
+audio_player_1 = AudioPlayer('192.168.1.110', 5000)
+audio_player_2 = AudioPlayer('192.168.1.111', 5000)
+audio_player_3 = AudioPlayer('192.168.1.112', 5000)
+all_audio_players = AudioPlayerGroup([audio_player_1, audio_player_2, audio_player_3])
+
+# WLED
+wled_1 = Wled('192.168.1.120')
+wled_2 = Wled('192.168.1.121')
+wled_3 = Wled('192.168.1.122')
+all_wleds = WledGroup([wled_1, wled_2, wled_3])
+
+# Smoke Modules
+smoke_module_1 = GPIOPin("192.168.1.130", 5)
+smoke_module_2 = GPIOPin("192.168.1.131", 5)
+smoke_module_3 = GPIOPin("192.168.1.132", 5)
+all_smoke_modules = GPIOPinGroup([smoke_module_1, smoke_module_2, smoke_module_3])
+
+# Triggers
+trigger_handler.add('motion_sensor_1')
+trigger_handler.add('motion_sensor_2')
+trigger_handler.add('motion_sensor_3')
+trigger_handler.add('doorbell_sensor')
+
+trigger_handler.add('smoke_module_1', deactivate_cooldown=True)
+trigger_handler.add_http_callback('smoke_module_1', 'smoke_module_1_callback', (smoke_module_1.turn_on_for, (5,)))
+
+trigger_handler.add('smoke_module_2', deactivate_cooldown=True)
+trigger_handler.add_http_callback('smoke_module_2', 'smoke_module_2_callback', (smoke_module_2.turn_on_for, (5,)))
+
+trigger_handler.add('smoke_module_3', deactivate_cooldown=True)
+trigger_handler.add_http_callback('smoke_module_3', 'smoke_module_3_callback', (smoke_module_3.turn_on_for, (5,)))
+
+trigger_handler.add('stop_smoke', deactivate_cooldown=True)
+trigger_handler.add_callback('stop_smoke', 'stop_smoke_callback', (all_smoke_modules.turn_off, ()))
+
+trigger_handler.add('stop_audio', deactivate_cooldown=True)
+trigger_handler.add_callback('stop_audio', 'stop_audio_callback', (all_audio_players.stop, ()))
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
