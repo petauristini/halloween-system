e237b68bab90ec3bb74db9855e109d8500d1cd76
AndrinWinzap <AndrinWinzap@proton.me>
2024-10-31 18:28:11 +0100

    update

--- speaker.py
+++ speaker.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from audioplayer.server import AudioPlayerServer
 from audiostreaming.output import AudioStreamingOutputServer
-from raspigpio.server import GPIOPinServer
+#from raspigpio.server import GPIOPinServer
 
 
 app = Flask(__name__)
 
 audio_player = AudioPlayerServer(app)
 streaming_output = AudioStreamingOutputServer(app)
-gpio_pin = GPIOPinServer(app, 2, "smoke")
+#gpio_pin = GPIOPinServer(app, 2, "smoke")
 
 app.run(debug=False, host='0.0.0.0', port=5000)
